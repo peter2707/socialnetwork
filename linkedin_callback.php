c7a4c8532d5710bef1d1c20b9218fe805d77eedd
Khaxtran <68594116+Khaxtran@users.noreply.github.com>
2024-05-03 11:49:21 +1000

    minor Login UI adjustment (#6)

--- linkedin_callback.php
+++ linkedin_callback.php
@@ -2,7 +2,7 @@
 require 'linkedin_provider.php';
 
 $clientId = '86lp9sdvq4owae';
-$clientSecret = ***REMOVED***;
+$clientSecret = 'vevCs1ULLQ476G3N';
 $redirectUri = 'http://localhost/oauth/linkedin_callback.php';
 
 $linkedinProvider = new LinkedInProvider($clientId, $clientSecret, $redirectUri);
