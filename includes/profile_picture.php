ea240df0f10f30a0014f0dccfd7e8df7ad2b053f
Kha Tran <khatran961217@gmail.com>
2024-05-08 17:53:58 +1000

    load user profile picture fetched from IDP
    added a new column to contain user_picture value in the database
    modified profile_picture.php to display fetched image
    updated linkedin_callback.php to accomodate the changes
    updated $profilesql to SELECT user_picture

--- includes/profile_picture.php
+++ includes/profile_picture.php
@@ -1,13 +1,18 @@
 <?php
 
-$target = glob("data/images/profiles/" . $row['user_id'] . ".*");
-if($target) {
-    echo '<img src="' . $target[0] . '" width="' . $width . '" height="' . $height .'">'; 
+if(!empty($row['user_picture'])) {
+    $image_url = $row['user_picture'];
+    echo '<img src="' . $image_url . '" width="' . $width . '" height="' . $height .'">'; 
 } else {
-    if($row['user_gender'] == 'M') {
-        echo '<img src="data/images/profiles/M.jpg" width="' . $width . '" height="' . $height .'">';
-    } else if ($row['user_gender'] == 'F') {
-        echo '<img src="data/images/profiles/F.jpg" width="' . $width . '" height="' . $height .'">';
+    $target = glob("data/images/profiles/" . $row['user_id'] . ".*");
+    if($target) {
+        echo '<img src="' . $target[0] . '" width="' . $width . '" height="' . $height .'">'; 
+    } else {
+        if($row['user_gender'] == 'M') {
+            echo '<img src="data/images/profiles/M.jpg" width="' . $width . '" height="' . $height .'">';
+        } else if ($row['user_gender'] == 'F') {
+            echo '<img src="data/images/profiles/F.jpg" width="' . $width . '" height="' . $height .'">';
+        }
     }
 }
 
--- linkedin_callback.php
+++ linkedin_callback.php
@@ -2,51 +2,58 @@
 require 'linkedin_provider.php';
 require 'functions/functions.php';
 session_start();
-
+ 
 $clientId = '86lp9sdvq4owae';
 $clientSecret = 'vevCs1ULLQ476G3N';
 $redirectUri = 'http://localhost/oauth/linkedin_callback.php';
-
+ 
 $linkedinProvider = new LinkedInProvider($clientId, $clientSecret, $redirectUri);
-
+ 
 if (isset($_GET['code'])) {
     $code = $_GET['code'];
     $accessToken = $linkedinProvider->getAccessToken($code);
-
+ 
     if ($accessToken) {
         $userProfile = $linkedinProvider->getUserProfile($accessToken);
-
+ 
         if ($userProfile) {
             $userEmail = $userProfile->email;
-
+ 
             // Check if the user email exists in the database
             $conn = connect();
             $query = mysqli_query($conn, "SELECT * FROM users WHERE user_email = '$userEmail'");
-
+ 
             if ($query) {
                 if (mysqli_num_rows($query) == 0) {
                     // User email doesn't exist, register the user
                     $userFirstName = $userProfile->given_name;
                     $userLastName = $userProfile->family_name;
-
+                    $userPicture = $userProfile->picture;
+ 
                     // Insert user into the database
-                    $sql = "INSERT INTO users (user_firstname, user_lastname, user_email) VALUES ('$userFirstName', '$userLastName', '$userEmail')";
+                    $sql = "INSERT INTO users (user_firstname, user_lastname, user_email, user_picture, user_password)
+                            VALUES ('$userFirstName', '$userLastName', '$userEmail', '$userPicture', '')";
                     $insertQuery = mysqli_query($conn, $sql);
-
+ 
                     if ($insertQuery) {
                         // User registered successfully, proceed with login
                         $userId = mysqli_insert_id($conn);
                         $_SESSION['user_id'] = $userId;
+                        echo $_SESSION['user_id'];
                         $_SESSION['user_name'] = $userFirstName . ' ' . $userLastName;
                         header("location: home.php");
                         exit(); // Exit to prevent further execution
                     } else {
                         // Error occurred while registering user
+                        printf("Query failed: %s\n", mysqli_connect_error());
                         echo "Error registering user. Please try again later.";
                     }
                 } else {
-                    // User email already exists, show error
-                    echo "User with this email already exists. Please login with your credentials.";
+                    // User email already exists, then login
+                    $row = mysqli_fetch_assoc($query);
+                    $_SESSION['user_id'] = $row['user_id'];
+                    $_SESSION['user_name'] = $row['user_firstname'] . " " . $row['user_lastname'];
+                    header("location:home.php");
                 }
             } else {
                 // Error occurred while checking email existence
